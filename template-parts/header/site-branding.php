82e81e883a71e5ebc962b026c585201f5cc8bae9
Dobrica Pavlinusic <dpavlin@rot13.org>
2025-06-15 13:03:03 +0200

    site logo left of site name and tagline

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -1,10 +1,9 @@
 <?php
 /**
- * Displays header site branding
+ * Displays header site branding (CHILD THEME OVERRIDE)
  *
  * @package WordPress
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
+ * @subpackage Twenty_Twenty_One_Child
  */
 
 $blog_info    = get_bloginfo( 'name' );
@@ -14,29 +13,33 @@ $header_class = $show_title ? 'site-title' : 'screen-reader-text';
 
 ?>
 
-<?php if ( has_custom_logo() && $show_title ) : ?>
-	<div class="site-logo"><?php the_custom_logo(); ?></div>
-<?php endif; ?>
+<div class="site-branding-wrapper">
 
-<div class="site-branding">
+    <?php if ( has_custom_logo() && $show_title ) : ?>
+        <div class="site-logo"><?php the_custom_logo(); ?></div>
+    <?php endif; ?>
 
-	<?php if ( has_custom_logo() && ! $show_title ) : ?>
-		<div class="site-logo"><?php the_custom_logo(); ?></div>
-	<?php endif; ?>
+    <div class="site-branding">
 
-	<?php if ( $blog_info ) : ?>
-		<?php if ( is_front_page() && ! is_paged() ) : ?>
-			<h1 class="<?php echo esc_attr( $header_class ); ?>"><?php echo esc_html( $blog_info ); ?></h1>
-		<?php elseif ( is_front_page() && ! is_home() ) : ?>
-			<h1 class="<?php echo esc_attr( $header_class ); ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo esc_html( $blog_info ); ?></a></h1>
-		<?php else : ?>
-			<p class="<?php echo esc_attr( $header_class ); ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo esc_html( $blog_info ); ?></a></p>
-		<?php endif; ?>
-	<?php endif; ?>
+        <?php if ( has_custom_logo() && ! $show_title ) : ?>
+            <div class="site-logo"><?php the_custom_logo(); ?></div>
+        <?php endif; ?>
 
-	<?php if ( $description && true === get_theme_mod( 'display_title_and_tagline', true ) ) : ?>
-		<p class="site-description">
-			<?php echo $description; // phpcs:ignore WordPress.Security.EscapeOutput ?>
-		</p>
-	<?php endif; ?>
-</div><!-- .site-branding -->
+        <?php if ( $blog_info ) : ?>
+            <?php if ( is_front_page() && ! is_paged() ) : ?>
+                <h1 class="<?php echo esc_attr( $header_class ); ?>"><?php echo esc_html( $blog_info ); ?></h1>
+            <?php elseif ( is_front_page() && ! is_home() ) : ?>
+                <h1 class="<?php echo esc_attr( $header_class ); ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo esc_html( $blog_info ); ?></a></h1>
+            <?php else : ?>
+                <p class="<?php echo esc_attr( $header_class ); ?>"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo esc_html( $blog_info ); ?></a></p>
+            <?php endif; ?>
+        <?php endif; ?>
+
+        <?php if ( $description && true === get_theme_mod( 'display_title_and_tagline', true ) ) : ?>
+            <p class="site-description">
+                <?php echo $description; // phpcs:ignore WordPress.Security.EscapeOutput ?>
+            </p>
+        <?php endif; ?>
+    </div><!-- .site-branding -->
+
+</div><!-- .site-branding-wrapper -->
